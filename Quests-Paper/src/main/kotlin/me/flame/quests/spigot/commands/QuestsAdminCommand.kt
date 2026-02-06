ff164c49003ca958aecb489c2f01fbb775f5bdca
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 21:54:11 +0200

    Skeleton

---
+++ Quests-Paper/src/main/kotlin/me/flame/quests/spigot/commands/QuestsAdminCommand.kt
@@ -0,0 +1,4 @@
+package me.flame.quests.spigot.commands
+
+class QuestsAdminCommand {
+}
--- Quests-Paper/src/main/kotlin/me/flame/quests/spigot/commands/QuestsCommand.kt
+++ Quests-Paper/src/main/kotlin/me/flame/quests/spigot/commands/QuestsCommand.kt
@@ -1,7 +1,10 @@
 package me.flame.quests.spigot.commands
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import me.flame.quests.api.database.QuestDatabase
 import me.flame.quests.api.database.QuestPlayerDatabase
+import me.flame.quests.spigot.QuestsPlugin
 
 import org.bukkit.entity.Player
 
@@ -9,11 +12,21 @@ import studio.mevera.imperat.annotations.Command
 import studio.mevera.imperat.annotations.Usage
 
 @Command(value = [ "quests" ])
-class QuestsCommand {
+class QuestsCommand(val plugin: QuestsPlugin) {
 
     @Usage
     fun onQuestsCommand(player: Player) {
+        val deferredData = plugin.async {
+            plugin.questsManager.getQuestPlayer(player.uniqueId)
+        }
 
+        val deferredQuests = plugin.async {
+            plugin.questsManager.getAllQuests()
+        }
 
+        plugin.launch {
+            val (data, quests) = deferredData.await() to deferredQuests.await()
+
+        }
     }
 }
