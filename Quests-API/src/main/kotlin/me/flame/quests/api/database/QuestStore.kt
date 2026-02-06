87dea050df882f73094d85f68616457ca0d9c4e8
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 21:53:23 +0200

    Fix import

--- Quests-API/src/main/kotlin/me/flame/quests/api/database/QuestStore.kt
+++ Quests-API/src/main/kotlin/me/flame/quests/api/database/QuestStore.kt
@@ -1,6 +1,6 @@
 package me.flame.quests.api.database
 
-import me.flame.quests.api.quest.QuestProgress
+import me.flame.quests.api.quest.progress.QuestProgress
 import me.flame.quests.api.quest.entity.QuestPlayer
 import java.util.UUID
 
