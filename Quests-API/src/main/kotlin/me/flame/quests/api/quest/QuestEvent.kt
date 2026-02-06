4793ab8e9454d4f7f137adaa13cb95ee4742ed20
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 22:08:56 +0200

    Make JvmRecord and fix import

--- Quests-API/src/main/kotlin/me/flame/quests/api/quest/QuestEvent.kt
+++ Quests-API/src/main/kotlin/me/flame/quests/api/quest/QuestEvent.kt
@@ -1,5 +1,8 @@
 package me.flame.quests.api.quest
 
+import me.flame.quests.api.quest.progress.QuestProgress
+
+@JvmRecord
 data class QuestEvent(
     val type: QuestEventType,
     val filters: MutableMap<String, String>,
