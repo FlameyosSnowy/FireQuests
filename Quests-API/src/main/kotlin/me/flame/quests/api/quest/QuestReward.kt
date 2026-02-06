1837ea14e831597879e80667cbb9cad66f6c3f5a
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 22:04:53 +0200

    Remove useless files

--- Quests-API/src/main/kotlin/me/flame/quests/api/quest/QuestReward.kt
+++
@@ -1,7 +0,0 @@
-package me.flame.quests.api.quest
-
-import org.bukkit.entity.Player
-
-interface QuestReward {
-    fun rewardPlayer(player: Player)
-}
--- Quests-Paper/src/main/kotlin/me/flame/quests/spigot/impl/quest_players.kt
+++
@@ -1,8 +0,0 @@
-package me.flame.quests.spigot.impl
-
-import me.flame.quests.api.quest.entity.QuestPlayer
-import org.bukkit.entity.Player
-
-fun Player.asQuestPlayer(): QuestPlayer {
-    return SpigotQuestPlayer(this)
-}
