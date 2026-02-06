695a83bc7eb89ed4f4885922136430274600390f
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 22:07:52 +0200

    Finish the database section entirely

--- Quests-API/src/main/kotlin/me/flame/quests/api/database/mongodb/MongoCredentials.kt
+++ Quests-API/src/main/kotlin/me/flame/quests/api/database/mongodb/MongoCredentials.kt
@@ -1,3 +1,4 @@
 package me.flame.quests.api.database.mongodb
 
-data class MongoCredentials(val connectionString: String)
+@JvmRecord
+data class MongoCredentials(val connectionString: String, val databaseName: String)
