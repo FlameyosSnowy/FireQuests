973fee7299beea3bb99a62570acd9391f0911ebe
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-06 22:08:22 +0200

    Finish buildItemStack

--- Quests-Paper/src/main/kotlin/me/flame/quests/spigot/config/GuiItemSection.kt
+++ Quests-Paper/src/main/kotlin/me/flame/quests/spigot/config/GuiItemSection.kt
@@ -1,9 +1,18 @@
 package me.flame.quests.spigot.config
 
 import hazae41.minecraft.kutils.bukkit.Config
-import io.github.mqzen.menus.Lotus
+import io.github.mqzen.menus.misc.itembuilder.ComponentItemBuilder
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.CustomModelData
+import me.flame.quests.spigot.hooks.parsePapi
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Material
 import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
+
+val SERIALIZER: MiniMessage = MiniMessage.miniMessage();
 
 class GuiItemSection(
     override var config: ConfigurationSection
@@ -14,7 +23,18 @@ class GuiItemSection(
     val flags by stringList("flags")
     val customModelData by int("custom-model-data")
 
-    fun getItemStack(): ItemStack {
-        Lotus
+    fun buildItemStack(player: Player): ItemStack {
+        val build = ComponentItemBuilder.modern(Material.valueOf(material))
+            .setDisplay(SERIALIZER.deserialize(parsePapi(player, name)))
+            .setLore(lore.map { SERIALIZER.deserialize(parsePapi(player, it)) })
+            .addFlags(*flags.map { ItemFlag.valueOf(it) }.toTypedArray())
+            .build()
+
+        val custom = CustomModelData.customModelData()
+            .addFloat(customModelData.toFloat())
+            .build()
+
+        build.setData(DataComponentTypes.CUSTOM_MODEL_DATA, custom)
+        return build
     }
 }
