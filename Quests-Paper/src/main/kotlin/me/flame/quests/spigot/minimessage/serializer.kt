7005871e8e05cdded698feae94444bed7f3dd3bc
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-08 13:15:38 +0200

    Add serializer utilities

---
+++ Quests-Paper/src/main/kotlin/me/flame/quests/spigot/minimessage/serializer.kt
@@ -0,0 +1,7 @@
+package me.flame.quests.spigot.minimessage
+
+import net.kyori.adventure.text.minimessage.MiniMessage
+
+val SERIALIZER: MiniMessage = MiniMessage.miniMessage();
+
+fun color(color: String) = SERIALIZER.deserialize(color)
