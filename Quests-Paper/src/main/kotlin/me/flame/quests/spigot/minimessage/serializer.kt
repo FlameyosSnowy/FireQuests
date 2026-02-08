5470b7e104a87f65632ff59a11f69daff902436f
flameyosflow <flamesfflowsnow@gmail.com>
2026-02-08 16:51:22 +0200

    Remove semicolon

--- Quests-Paper/src/main/kotlin/me/flame/quests/spigot/minimessage/serializer.kt
+++ Quests-Paper/src/main/kotlin/me/flame/quests/spigot/minimessage/serializer.kt
@@ -2,6 +2,6 @@ package me.flame.quests.spigot.minimessage
 
 import net.kyori.adventure.text.minimessage.MiniMessage
 
-val SERIALIZER: MiniMessage = MiniMessage.miniMessage();
+val SERIALIZER: MiniMessage = MiniMessage.miniMessage()
 
 fun color(color: String) = SERIALIZER.deserialize(color)
